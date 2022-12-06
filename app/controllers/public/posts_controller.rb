884f74ed48ac3908fb724b41b196c760182df303
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2022-12-06 14:15:53 +0000

    タグ追加機能&下書き機能仮作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,5 +1,6 @@
 class Public::PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  
   def new
     @post = Post.new
   end
@@ -11,17 +12,19 @@ class Public::PostsController < ApplicationController
   def show
     @post= Post.find(params[:id])
     @post_comment = Comment.new
+    @post_tags = @post.tags
   end
 
   def edit
     @post= Post.find(params[:id])
+    @post_tags = @post.tags.pluck(:tag_name).split(nil)
   end
   
   def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id
-    @post.post_tag_id = current_user.id #タグ機能作成後、変更すること
+    @post = current_user.posts.new(post_params)
+    tag_list = params[:post][:tag_name].split(nil)
       if @post.save
+        @post.save_tag(tag_list)
         redirect_to post_path(@post.id)
       else
       render :new, notice: "登録できませんでした。お手数ですが、入力内容をご確認のうえ再度お試しください。"
@@ -29,8 +32,16 @@ class Public::PostsController < ApplicationController
   end
   
   def update
+    #postのidを取得
     @post= Post.find(params[:id])
+    #入力されたタグの受け取り
+    tag_list = params[:post][:tag_name].split(nil)
     if @post.update(post_params)
+      old_relations = PostTag.where(post_id: @post.id)
+      old_relations.each do |relation|
+        relation.delete
+      end
+      @post.save_tag(tag_list)
       redirect_to post_path(@post.id), notice: "更新しました。"
     else
       render :edit
@@ -44,6 +55,7 @@ class Public::PostsController < ApplicationController
   end
   
   def draft
+    @posts = current_user.posts
   end
   
   def draft_edit
@@ -55,6 +67,6 @@ class Public::PostsController < ApplicationController
   private
   
   def post_params
-    params.require(:post).permit(:name, :place, :explanation, :user_id, :post_tag_id, :category_id, :is_active, :draft_status, post_image: [])
+    params.require(:post).permit(:name, :place, :explanation, :user_id, :category_id, :is_active, :draft_status, post_image: [])
   end
 end
