fa0c8ce545389d2b28d0073275250d04429c835e
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2022-12-07 05:30:22 +0000

    Admin User&Post一覧、詳細、機能実装

---
+++ app/controllers/admin/comments_controller.rb
@@ -0,0 +1,29 @@
+class Admin::CommentsController < ApplicationController
+ 
+ def index
+  @comments = Comment.all
+ end
+    
+ def create
+ end
+ 
+ def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to post_path(params[:post_id])
+ end
+ 
+ def update
+  post = Post.find(params[:post_id])
+  comment = Comment.find(params[:id])
+  comment.comment_content = "不適切なコメントの為、削除されました。"
+  if comment.save
+  redirect_to admin_post_path(post), notice: "コメントを削除しました。"
+  end
+ end
+ 
+ private
+ 
+ def post_comment_params
+  params.require(:comment).permit(:comment_content)
+ end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2022_12_03_105552) do
   end
 
   create_table "admins", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
