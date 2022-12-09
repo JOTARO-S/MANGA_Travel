2fc3f3cb30781754c9a16cf3118c74241bb2009e
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2022-12-09 16:07:03 +0000

    ゲストログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,4 +28,11 @@ class User < ApplicationRecord
       @user = User.where("name LIKE?", "%#{word}%")
   end
   
+  def self.guest
+    find_or_create_by!(name: "guestuser", email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+  
 end
