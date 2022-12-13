fef9197004c2659d39bb2af307d6d6e83a00eb01
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2022-12-13 08:10:25 +0000

    投稿一覧画面レイアウト

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -1,5 +1,5 @@
 class Public::ChatsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
   
   def new
     @chat = Chat.new
