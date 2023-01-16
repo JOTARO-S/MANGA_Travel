fd72ce9edccc224b337ca14d00153844349f05b4
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2023-01-16 10:34:11 +0000

    route修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  
-  get 'relationships/followings'
-  get 'relationships/followers'
+
 # 顧客用
 devise_for :user, controllers: {
   registrations: "public/registrations",
