0826627f701d71ae445995b54a85f0137eb4a947
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2023-01-16 10:56:28 +0000

    レイアウト調整

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -13,13 +13,13 @@ class Public::RelationshipsController < ApplicationController
   end
   # フォロー一覧
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings.page(params[:page]).per(12)
+    @user = User.find(params[:user_id])
+    @users = @user.followings.page(params[:page]).per(12)
   end
   # フォロワー一覧
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers.page(params[:page]).per(12)
+    @user = User.find(params[:user_id])
+    @users = @user.followers.page(params[:page]).per(12)
   end
   
 end
