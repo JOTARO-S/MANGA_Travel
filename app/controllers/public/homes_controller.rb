5faf06319297b13106be7cc2dcc95d0de1d4f257
JOTARO-S <88357567+JOTARO-S@users.noreply.github.com>
2023-01-25 03:41:26 +0000

    ブックマーク、TOP修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
-    to = Time.current.at_end_of_day
-    from = (to - 6.day).at_beginning_of_day
 
-    @posts = Post.where(created_at: from...to).find(Bookmark.group(:post_id).order('count(post_id) desc').limit(8).pluck(:post_id))
+    @posts = Post.find(Bookmark.group(:post_id).order('count(post_id) desc').limit(8).pluck(:post_id))
     @tags = Tag.find(PostTag.group(:tag_id).order('count(post_id) desc').limit(10).pluck(:tag_id))
     
   end
